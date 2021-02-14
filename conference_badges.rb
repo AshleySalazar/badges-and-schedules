15fffab9ad1a6d1a06bf07fd7ecb88d48163ad5c
Ashley Salazar <ashleysalazar0902@gmail.com>
2021-02-13 20:59:02 -0600

    commit tree d8012a6b8a6283a5d2d7dd3f8519b6b21abf9657

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,32 @@
-# Write your code here.
+def badge_maker(name)
+    return "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(names)
+    badges = []
+
+    names.each do |name|
+        badges << badge_maker(name)
+    end
+
+    return badges
+end
+
+def assign_rooms(speakers)
+    rooms = speakers.collect.with_index{ |speaker, index| "Hello, #{speaker}! You'll be assigned to room #{index + 1}!"}
+
+    return rooms
+end
+
+def printer(names)
+    badges = batch_badge_creator(names)
+    rooms = assign_rooms(names)
+
+    badges.each do |badge|
+        puts badge
+    end
+
+    rooms.each do |message|
+        puts message
+    end
+end
